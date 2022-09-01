f59cde6ec9f140d4e7f18386f6d9d69f247631a9
sayyyyyy <d958956a6b650@gmail.com>
2022-09-01 21:22:31 +0900

    ステータス画面修正

--- MuscleOnline/Assets/Scripts/Home/ExpScript.cs
+++ MuscleOnline/Assets/Scripts/Home/ExpScript.cs
@@ -7,50 +7,13 @@ using UnityEngine.UI;
 public class ExpScript : MonoBehaviour
 {
     [SerializeField] Image userExpImage;
-    float userExp;
-    float userMaxExp;
     float userExpDivision;
 
-    // Start is called before the first frame update
     void Start()
     {
-        userExpImage = GetComponent<Image>();
-
-        //userExp = (float)UserInfo.UserExp;
-        userExp = 10;
-        //userMaxExp = (float)UserInfo.UserMaxExp;
-        userMaxExp = 20;
-        Debug.Log("userExp" + userExp);
-        Debug.Log("userMaxExp" + userMaxExp);
-
-
-
-        userExpDivision = userExp / userMaxExp;
-        Debug.Log("userExpDivision" + userExpDivision);
-        UpdateUserExp(userExpDivision);
-    }
-
-    //private void Update()
-    //{
-    //    ////userExp = (float)UserInfo.UserExp;
-    //    //userExp = 10;
-    //    ////userMaxExp = (float)UserInfo.UserMaxExp;
-    //    //userMaxExp = 20;
-    //    //Debug.Log("userExp" + userExp);
-    //    //Debug.Log("userMaxExp" + userMaxExp);
-
-
-
-    //    //userExpDivision = userExp / userMaxExp;
-    //    //Debug.Log("userExpDivision" + userExpDivision);
-    //    //UpdateUserExp(userExpDivision);
-
-    //}
-
-    //fillAmountの値を変更する関数
-    public void UpdateUserExp(float userExpDivision)
-    {
+        userExpDivision = UserInfo.UserExp / UserInfo.UserMaxExp;
         userExpImage.fillAmount = userExpDivision;
     }
+    //fillAmountの値を変更する関数
 
 }
