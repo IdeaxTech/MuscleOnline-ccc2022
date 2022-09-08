e636ef9954c2d005b776f8455c8e962afb8738a4
hibiki0612 <bee.hibiki12@gmail.com>
2022-09-08 21:32:36 +0900

    QUQUの追加

--- MuscleOnline/Assets/Enemy/Scripts/EnemyCreanHitBehaviour.cs
+++ MuscleOnline/Assets/Enemy/Scripts/EnemyCreanHitBehaviour.cs
@@ -21,7 +21,7 @@ public class EnemyCreanHitBehaviour : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.ResetTrigger("Hit");
+        animator.ResetTrigger("CreanHit");
 
     }
 
